f99b55446bb12d83e732e9d0f02766db0d05a8e0
Mikej81 <stumpj@gmail.com>
2020-04-01 18:11:33 -0400

    update

--- install.sh
+++ install.sh
@@ -9,6 +9,19 @@ az aks get-credentials --resource-group myResourceGroup --name juiceCluster
 ##
 kubectl get nodes
 ##
+elm install stable/nginx-ingress \
+    --namespace ingress-basic \
+    -f internal-ingress.yaml \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
+##
+kubectl create namespace ingress-basic
+##
+kubectl apply -f ingress.yaml
+##
+kubectl get all -n ingress-nginx
+##
 kubectl apply -f juiceshop-aks.yaml
 ##
 kubectl get service juiceshop-public
