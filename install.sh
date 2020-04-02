3fb0073b688bfadd75a9b6ab72a722c31e5288ee
Mikej81 <stumpj@gmail.com>
2020-04-02 09:16:20 -0400

    cleanup

--- install.sh
+++ install.sh
@@ -9,27 +9,26 @@ az aks get-credentials --resource-group myResourceGroup --name juiceCluster
 ## create the namespace
 kubectl create namespace juiceshop
 # Add the official stable repositories
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm repo add multi-juicer https://iteratec.github.io/multi-juicer/
+#helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+#helm repo add multi-juicer https://iteratec.github.io/multi-juicer/
 ## install nginx ingress controller
 helm install nginx-ingress stable/nginx-ingress \
     --namespace juiceshop \
-    -f internal-ingress.yaml \
     --set controller.replicaCount=2 \
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
 ##
 #kubectl get service -l app=nginx-ingress --namespace juiceshop
-helm install multi-juicer multi-juicer/multi-juicer --namespace juiceshop \
-    --set title="AKS Juiceshop Primary" \
-    --set serviceName="juiceshop1"
-helm install multi-juicer multi-juicer/multi-juicer --namespace juiceshop \
-    --set title="AKS Juiceshop Secondary" \
-    --set serviceName="juiceshop2"
+#helm install multi-juicer multi-juicer/multi-juicer --namespace juiceshop \
+#    --set title="AKS Juiceshop Primary" \
+#    --set serviceName="juiceshop1"
+#helm install multi-juicer multi-juicer/multi-juicer --namespace juiceshop \
+#    --set title="AKS Juiceshop Secondary" \
+#    --set serviceName="juiceshop2"
 ##
 kubectl get all -n juiceshop
 ##
-#kubectl apply -f juiceshop-aks.yaml
-kubectl apply -f juicer-ingress.yaml
+kubectl apply -f juiceshop-aks.yaml
+#kubectl apply -f juicer-ingress.yaml
 ##
 #kubectl get service
